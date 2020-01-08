884b151fc868cdc212b73ffe7ad7f3efb93caa19
Daichi Kudo <sakanactor624@gmail.com>
2020-01-08 19:40:53 +0900

    :sparkles: 英単語テスト作成機能追加

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# 英単語を日本語化するアプリ
+# 英単語を日本語に訳して単語テストを作成するツール
 
-- googletransの仕様で一気に処理するとリジェクトされる
+## 使い方
+
+- `git clone git@github.com:Kudoas/words_translater.git`でローカルにファイルをダウンロードする。
+- ローカルのpython環境に`pip3 install googletrans`を入れるか`docker-compose up -d`で起動する環境を作る
+- テスト作成用のファイルはtxt形式で`英単語のみ`か`1.	abide`のような形で一行ずつ用意する
+
+```txt
+例
+apple
+fish
+
+1. apple
+2. fish
+```
+
+- テスト作成用のファイルをenglish-wordsフォルダにwords.txtという名前で入れる
+
+-  `python src/main.py`をコマンドに打つ
+
+- outputに翻訳後のCSVファイルとtestfileに単語テストが自動生成される
+
+## 注意点
+
+- googletransの仕様で多数の英単語を翻訳するとリジェクトされる(デフォルトは100単語)
--- src/main.py
+++ src/main.py
@@ -1,36 +1,87 @@
 import csv
-import re
+import random
 from googletrans import Translator
 
 
 class WordsTranslater():
     english_words = []
-    japanese_words = []
+    meanings = []
 
     def create_words_list(self, source='english-words/words.txt'):
         with open(source, newline='') as f:
             reader = f.readlines()
             for row in reader:
                 row = row.split()
-                self.english_words.append(row[1])
+                if len(row) == 1:
+                    self.english_words.append(row[0])
+                elif len(row) == 2:
+                    self.english_words.append(row[1])
 
-    # 1000語を一気にやろうとするとリジェクトされる
-    def translate_words(self, n=200):
+    def shuffle_words_list(self):
+        random.shuffle(self.english_words)
+
+    # googletransの仕様で一気に多くの単語を処理できない
+    def translate_words(self, n=100):
         for i in range(n):
             translator = Translator()
             trans_de_ja = translator.translate(
                 self.english_words[i], src='en', dest='ja'
             )
-            self.japanese_words.append(trans_de_ja.text)
+            self.meanings.append(trans_de_ja.text)
 
     def write_csv(self, output_file='output/output.csv'):
         with open(output_file, 'w', newline='') as f:
             writer = csv.writer(f)
-            for s, t in zip(self.english_words, self.japanese_words):
+            for s, t in zip(self.english_words, self.meanings):
                 writer.writerow([s, t])
 
 
+class WordsTestCreater():
+    # 作りたいテストの数、問題数を指定
+    num_tests = 5
+    num_questions = 5
+
+    wrong_options = 3
+    words_dict = {}
+
+    def create_words_dict(self, source='output/output.csv'):
+        with open(source, newline='') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                self.words_dict[row[0]] = row[1]
+
+    def create_words_test(self):
+        for i in range(1, self.num_tests+1):
+            with open('testfile/第{:02d}回: 英単語テスト'.format(i), 'w') as f:
+                f.write('第{:02d}回: 英単語テスト'.format(i))
+                f.write('\n\n')
+
+                for num_question in range(self.num_questions):
+                    quetion_word = random.choice(list(self.words_dict.keys()))
+                    correct_answer = self.words_dict[quetion_word]
+
+                    copy_meanings = list(self.words_dict.values()).copy()
+                    copy_meanings.remove(correct_answer)
+                    wrong_answers = random.sample(
+                        copy_meanings, self.wrong_options
+                    )
+                    answer_options = [correct_answer] + wrong_answers
+                    random.shuffle(answer_options)
+
+                    f.write('問{}. {}'.format(num_question + 1, quetion_word))
+                    f.write('\n')
+                    for i in range(4):
+                        f.write('{}. {}\n'.format(i+1, answer_options[i]))
+                    f.write('\n\n')
+
+
 words_translater = WordsTranslater()
-words_translater.create_words_list()
-words_translater.translate_words()
-words_translater.write_csv()
+test_creater = WordsTestCreater()
+
+if __name__ == "__main__":
+    words_translater.create_words_list()
+    words_translater.shuffle_words_list()
+    words_translater.translate_words()
+    words_translater.write_csv()
+    test_creater.create_words_dict()
+    test_creater.create_words_test()
